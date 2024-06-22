75803b0bdfb0517fd4431f10451cd6799c72451b
goosebbeoms <seo980916@gmail.com>
2024-06-23 01:22:31 +0900

    재생 149. “새 프로젝트” 컴포넌트와 재사용 가능한 “입력” 컴포넌트 추가하기

---
+++ src/components/Input.jsx
@@ -0,0 +1,9 @@
+const Input = ({ label, textarea, ...props }) => {
+  return (
+    <p>
+      <label>{label}</label>;{textarea ? <textarea {...props} /> : <input {...props} />}
+    </p>
+  );
+};
+
+export default Input;
