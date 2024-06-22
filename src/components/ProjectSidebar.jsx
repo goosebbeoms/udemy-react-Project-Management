da56c605f7d75c558306adf76d297770bf03544f
goosebbeoms <seo980916@gmail.com>
2024-06-23 00:33:33 +0900

    147. “프로젝트 사이드바” 컴포넌트 추가하기

---
+++ src/components/ProjectSidebar.jsx
@@ -0,0 +1,18 @@
+
+const ProjectSidebar = () => {
+  return (
+    <side>
+      <h2>Your Projects</h2>
+      <div>
+        <button>
+          + Add Project
+        </button>
+      </div>
+      <ul>
+        
+      </ul>
+    </side>
+  );
+};
+
+export default ProjectSidebar;
