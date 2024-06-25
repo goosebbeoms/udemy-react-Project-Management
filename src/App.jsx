99adf409c166748f02b8ff583f6fde9281a3c22f
goosebbeoms <seo980916@gmail.com>
2024-06-26 02:28:09 +0900

    153. Refs(참조)와 전달된 Refs(참조)로 사용자 입력 받아오기

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   let content
 
   if (projectsState.selectedProjectId === null) {
-    content = <NewProject />
+    content = <NewProject onAdd={handleAddProject} />
   } else if (projectsState.selectedProjectId === undefined) {
     content = <NoProjectSelected onStartAddProject={handleStartAddProject} />
   }
@@ -27,6 +27,22 @@ function App() {
     })
   }
 
+  function handleAddProject(projectData) {
+    const newProject = {
+      ...projectData,
+      id: Math.random()   // 같은 숫자가 두 번 이상 나올 수 있기 때문에 완벽하지는 않지만 연습 단계에서는 일단 사용
+    }
+
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        projects: [...prevState.projects, newProject]
+      }
+    })
+  }
+
+  console.log(projectsState)
+  
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectSidebar onStartAddProject={handleStartAddProject} />
