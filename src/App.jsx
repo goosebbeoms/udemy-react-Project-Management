a65306545bd0afa48888517ff5214fc5b5688547
goosebbeoms <seo980916@gmail.com>
2024-06-26 15:28:16 +0900

    154. 프로젝트 생성 핸들링 & UI 업데이트

--- src/App.jsx
+++ src/App.jsx
@@ -28,24 +28,24 @@ function App() {
   }
 
   function handleAddProject(projectData) {
-    const newProject = {
-      ...projectData,
-      id: Math.random()   // 같은 숫자가 두 번 이상 나올 수 있기 때문에 완벽하지는 않지만 연습 단계에서는 일단 사용
-    }
-
     setProjectsState((prevState) => {
+      const newId = Math.random()   // 같은 숫자가 두 번 이상 나올 수 있기 때문에 완벽하지는 않지만 연습 단계에서는 일단 사용
+      const newProject = {
+        ...projectData,
+        id: newId,
+      }
+
       return {
         ...prevState,
+        selectedProjectId: undefined,
         projects: [...prevState.projects, newProject]
       }
     })
   }
 
-  console.log(projectsState)
-  
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSidebar onStartAddProject={handleStartAddProject} />
+      <ProjectSidebar onStartAddProject={handleStartAddProject} projects={projectsState.projects} />
       {content}
     </main>
   );
