59a27b16a5f8b548f983bbc8dcf8887f68be04a6
goosebbeoms <seo980916@gmail.com>
2024-06-28 00:10:57 +0900

    159. “프로젝트 태스크” 및 태스크 컴포넌트 추가

--- src/components/SelectedProject.jsx
+++ src/components/SelectedProject.jsx
@@ -1,3 +1,5 @@
+import Tasks from "./Tasks";
+
 const SelectedProject = ({project, onDelete}) => {
   const formattedDate = new Date(project.dueDate).toLocaleDateString('ko-KR', {
     year: 'numeric',
@@ -15,6 +17,7 @@ const SelectedProject = ({project, onDelete}) => {
         <p className="mb-4 text-stone-400">{formattedDate}</p>
         <p className="text-stone-600 whitespace-pre-wrap">{project.description}</p>
       </header>
+      <Tasks />
     </div>
   );
 };
