51fb8efe4c064f45a85ddeb1ed9e54748f08c3e4
goosebbeoms <seo980916@gmail.com>
2024-06-27 17:04:48 +0900

    158. 프로젝트 삭제 핸들링

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
 
   const selectedProject = projectsState.projects.find(project => project.id === projectsState.selectedProjectId)
 
-  let content = <SelectedProject project={selectedProject} />
+  let content = <SelectedProject project={selectedProject} onDelete={handleDeleteProject} />
 
   if (projectsState.selectedProjectId === null) {
     content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />
@@ -61,6 +61,16 @@ function App() {
         ...prevState,
         selectedProjectId: undefined,
         projects: [...prevState.projects, newProject]
+      }
+    })
+  }
+
+  function handleDeleteProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter((project) => project.id !== prevState.selectedProjectId)
       }
     })
   }
